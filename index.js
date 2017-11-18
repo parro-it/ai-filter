81dee2a8cc08f9618b83ad767bccc520bb84445a
Andrea Parodi <andrea@parro.it>
2017-11-18 17:49:24 +0100

    docs

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import isAsyncIterable from "is-async-iterable";
 
+/**
+ * The filter() method creates a new async iterable
+ * with all elements that pass the test implemented
+ * by the provided function.
+ *
+ * @param {Function} predicate
+ * is a predicate, to test each element of the async
+ * iterable. Return true to keep the element, false
+ * otherwise, taking three arguments:
+ *      - element - The current element being processed in the async iterable.
+ *      - index - The index of the current element being processed in the async iterable.
+ *      - iterable - The async iterable filter was called upon.
+ * @param {AsyncIterable} data The source async iterable to filter.
+ * @return {AsyncIterable} A new async iterable with the elements that pass the test.
+ */
 export default async function* filter(predicate, data) {
   if (typeof predicate !== "function") {
     throw new TypeError("predicate argument must be a function.");
